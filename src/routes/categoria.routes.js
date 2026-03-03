dbf0abbf7dfe3cde9c2f6083efd67627c58bdf08
BeatrizAmaral08 <bisandron@gmail.com>
2026-03-03 10:10:56 -0300

    Delete src directory

--- src/routes/categoria.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import categoriaController from "../controllers/controller.js";
-const categoriaRoutes = Router();
-
-categoriaRoutes.post('/categoria', produtoController.criarCategoria);
-categoriaRoutes.get('/categoria', produtoController.listarCategoria);
-
-export default categoriaRoutes;
